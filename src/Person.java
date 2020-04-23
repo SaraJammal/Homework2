545ff3778d92d7f853116eb9b813fdcee8e136e0
Sara Al-Jammal <sara.aljammal97@gmail.com>
2020-04-23 16:10:41 +0300

    updated person class

--- src/Person.java
+++ src/Person.java
@@ -3,12 +3,13 @@ public class Person {
     private String firstName;
     private String lastName;
     private int age;
-
+    private String hairColor;
     public Person()
     {
         this.firstName = "First Name";
         this.lastName = "Last Name";
         this.age = 0;
+        this.hairColor = "Black";
     }
     public Person(String firstName, String lastName, int age)
     {
@@ -44,11 +45,11 @@ public class Person {
 
     public void printName()
     {
-       System.out.println(firstName + " " + lastName);
+       System.out.println("Hello this is " + firstName + " " + lastName);
     }
     public void printAge()
     {
-        System.out.println(age);
+        System.out.println("My age is" + age);
     }
 
 }
